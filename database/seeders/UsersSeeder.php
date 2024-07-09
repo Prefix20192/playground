678e4e301ab56af01e7b257e2fdea62c01e32e3a
Kartav Nikita <prefix62221@mail.ru>
2024-07-09 15:23:51 +0300

    fix seed

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -17,6 +17,7 @@ class UsersSeeder extends Seeder
         if (User::query()->count() === 0) {
             User::factory()->create([
                 'name'                 => 'Test user',
+                'surname'              => 'Test surname',
                 'email'                => 'example@example.com',
                 'email_verified_at'    => now(),
                 'password'             => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
