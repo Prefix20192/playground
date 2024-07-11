cd57336dcfa77a71e1fd7bbda31281cbd331068a
Kartav Nikita <prefix62221@mail.ru>
2024-07-11 15:01:43 +0300

    fix: Corrected the action on the password recovery

--- app/Actions/User/RecoveryPasswordAction.php
+++ app/Actions/User/RecoveryPasswordAction.php
@@ -3,18 +3,28 @@
 namespace App\Actions\User;
 
 use App\DTO\User\RecoveryDTO;
+use App\Exceptions\Api\User\RecoveryException;
+use App\Models\PasswordResetToken;
 use App\Models\User;
+use App\Jobs\Api\User\SendRecoveryPasswordEmail;
+use Illuminate\Support\Str;
 
 class RecoveryPasswordAction
 {
     public function __invoke(RecoveryDTO $recoveryDTO)
     {
         $user = User::where('email', $recoveryDTO->email)->first();
-        if(!$user)
-        {
-            //NOT-EXIST
+        if(!$user) {
+            throw new RecoveryException("User not found", 404);
         }
-        // Создать очередь на отправку уведомление о восстановление пароля
 
+        $token = Str::random(60);
+
+        PasswordResetToken::create([
+            'user_id' => $user->id,
+            'token' => $token,
+        ]);
+
+        SendRecoveryPasswordEmail::dispatch($user, $token)->onQueue('default');
     }
 }
